a4dca2f5fc9885800b759e296962dd5893c78a07
Joseph Louise <josephdeleonpogi@gmail.com>
2025-10-04 03:49:34 +0800

    remove unsued and fix error path, live deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,34 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
-const __filename = fileURLToPath(
-  import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = {
-  plugins: {
-    "jsx-a11y": require("eslint-plugin-jsx-a11y")
-  },
-  extends: [
+const eslintConfig = [
     ...compat.extends(
-      "next/core-web-vitals",
-      "next/typescript",
-      "plugin:jsx-a11y/recommended"
-    )
-  ],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "error",
-    "jsx-a11y/label-has-for": ["error", {
-      required: {
-        some: ["nesting", "id"]
-      }
-    }]
-  }
-};
+        "next/core-web-vitals",
+        "next/typescript",
+        "plugin:jsx-a11y/recommended"
+    ),
+    {
+        plugins: {
+            "jsx-a11y": jsxA11y
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off", // Changed to "off" to allow any type
+            "jsx-a11y/label-has-for": ["error", {
+                required: {
+                    some: ["nesting", "id"]
+                }
+            }]
+        }
+    }
+];
 
 export default eslintConfig;
--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,13 @@
 "use client"
 import UserResetPassword from '@/components/auth/UserResetPassword'
-import React from 'react'
+import { Loader2 } from 'lucide-react'
+import React, { Suspense } from 'react'
 
 const ResetPasswordPage = () => {
     return (
-        <UserResetPassword />
+        <Suspense fallback={<Loader2 className="animate-spin -ml-1 mr-3 h-5 w-5" />}>
+            <UserResetPassword />
+        </Suspense>
     )
 }
 
